5043df16aa03deba9ed8c52f42887c61be2a937b
unknown <durgaprasadchinta1997@gmail.com>
2022-04-22 21:50:02 +0530

    added jr ntr image

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
       <h2>siva krishna siriki</h2>
       <h1>prasad</h1>
       <h1>varaprasad</h1>
+      <img src='https://i.pinimg.com/736x/db/8a/55/db8a55d19c46baed2f9d005c8455a650--telugu-cinema-nuest-jr.jpg'></img>
     </div>
   );
 }
