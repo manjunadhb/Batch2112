eb40453df081449a6cb4f4ed459190083b59fd37
sirikisiva <sirikisivakrishna@gmail.com>
2022-04-22 17:38:46 +0530

    added name

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
       <h1>Names of BRN Batch 2112</h1>
       <h2>Manjunadh</h2>
       <h2 style={{color:"red"}}>mirza arif ali baig</h2>
+      <h2>siva krishna siriki</h2>
     </div>
   );
 }
