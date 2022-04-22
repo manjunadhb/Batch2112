b6ce58d2c2f8cdfdc9c53e3b65e213d0c5970f8d
varaprasad <svprasad347@gmail.com>
2022-04-22 17:47:57 +0530

    added varaprasad

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <h1>Names of BRN Batch 2112</h1>
       <h2  style={{color:"red"}}>Manjunadh</h2>
       <h2 style={{color:"red"}}>mirza arif ali baig</h2>
-      <h1>prasad</h1>
+      <h1>varaprasad</h1>
     </div>
   );
 }
