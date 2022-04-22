840faa9d3eac8d11daee6284d3023f231e366bcb
arif <mirzaarifali3@gmail.com>
2022-04-22 17:32:04 +0530

    color added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <h1>Names of BRN Batch 2112</h1>
       <h2>Manjunadh</h2>
-      <h2>mirza arif ali baig</h2>
+      <h2 style={{color:"red"}}>mirza arif ali baig</h2>
     </div>
   );
 }
